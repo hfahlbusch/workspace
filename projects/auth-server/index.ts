fafb9db48c1dc01504868633ddf0cc98a4d06201
Florian Tischler <florian.tischler@gmx.net>
2018-11-27 23:15:31 +0100

    Added flights route

--- projects/auth-server/index.ts
+++ projects/auth-server/index.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser());
@@ -20,6 +22,17 @@ app.get('/api/auth', (req: Request, res: Response) => {
     const token = req.header('authorization').split(' ')[1];
     if (token === jwt) {
       res.status(200).end();
+    }
+  }
+  res.status(401).end();
+});
+
+app.get('/api/flight', (req: Request, res: Response) => {
+  if (req.header('authorization')) {
+    const token = req.header('authorization').split(' ')[1];
+    if (token === jwt) {
+      const flights = JSON.parse(fs.readFileSync(path.join(__dirname, 'flights.json')).toString('utf8'));
+      res.json(flights);
     }
   }
   res.status(401).end();
