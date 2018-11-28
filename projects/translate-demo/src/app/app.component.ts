040c25a10b9c1aa0be28a3ee566ded594269d08c
Florian Tischler <florian.tischler@gmx.net>
2018-11-28 22:15:51 +0100

    livedemo finished

--- projects/translate-demo/src/app/app.component.ts
+++ projects/translate-demo/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'translate-demo';
+export class AppComponent implements OnInit, OnDestroy {
+  public languageFormGroup = new FormGroup({
+    language: new FormControl('en'),
+    name: new FormControl('')
+  });
+
+  private destroy$ = new Subject<boolean>();
+
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang('en');
+    translateService.use('en');
+  }
+
+  ngOnInit(): void {
+    this.languageFormGroup.get('language').valueChanges.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(lang => this.translateService.use(lang));
+  }
+
+  public greet(): void {
+    this.translateService.get('app.alerts.hello', {
+      name: this.languageFormGroup.get('name').value
+    }).pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(translation => {
+      alert(translation);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+  }
 }
--- projects/translate-demo/src/app/app.module.ts
+++ projects/translate-demo/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ReactiveFormsModule } from '@angular/forms';
+
+export function createTranslateLoader(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
